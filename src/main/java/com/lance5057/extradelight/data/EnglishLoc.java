618e53934f7cda0301d8d9eb0672ff6042691b52
alanx <alanxiao44@gmail.com>
2023-06-01 21:54:26 -0400

    Remove duplicate model files and add translation keys

--- src/main/java/com/lance5057/extradelight/data/EnglishLoc.java
+++ src/main/java/com/lance5057/extradelight/data/EnglishLoc.java
@@ -5,10 +5,7 @@ import com.lance5057.extradelight.ExtraDelightItems;
 import com.lance5057.extradelight.TranslatableKeys;
 import com.lance5057.extradelight.aesthetics.AestheticBlocks;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.common.data.LanguageProvider;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class EnglishLoc extends LanguageProvider {
 
@@ -308,15 +305,6 @@ public class EnglishLoc extends LanguageProvider {
 
 		this.add(ExtraDelightItems.BAD_FOOD.get(), "Bad Food");
 
-		add(ExtraDelightItems.OVEN.get(), "Oven");
-		add(ExtraDelightItems.SHEET.get(), "Baking Sheet");
-		add(ExtraDelightItems.TRAY.get(), "Baking Tray");
-		add(ExtraDelightItems.LOAF_PAN.get(), "Loaf Pan");
-		add(ExtraDelightItems.PIE_DISH.get(), "Pie Dish");
-		add(ExtraDelightItems.ROUND_PAN.get(), "Round Pan");
-		add(ExtraDelightItems.SQUARE_PAN.get(), "Square Pan");
-		add(ExtraDelightItems.BAKING_STONE.get(), "Baking Stone");
-		add(ExtraDelightItems.MUFFIN_TIN.get(), "Muffin Tin");
 		add(TranslatableKeys.OVEN_CONTAINER, "Baking Oven");
 		add(TranslatableKeys.OVEN_SERVED_ON, "Served on %s");
 		add(TranslatableKeys.MANY_SERVINGS, "Holds %s servings of:");
@@ -324,10 +312,5 @@ public class EnglishLoc extends LanguageProvider {
 		add(TranslatableKeys.EMPTY, "Empty");
 
 		AestheticBlocks.EngLoc(this);
-	}
-
-	private void add(MenuType<?> key, String name) {
-		ResourceLocation menuKey = ForgeRegistries.MENU_TYPES.getKey(key);
-		add("%s.container.%s".formatted(menuKey.getNamespace(), menuKey.getPath()), name);
 	}
 }
