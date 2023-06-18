17f637a5e9f13cc08b0ae9920fddee37ddb0038e
_0Steven <jaydondegenerschool@gmail.com>
2023-06-18 22:25:38 +0200

    Add breaking tools to feast blocks Mimicking base FD, all plated feasts&pies use knives Pots and pans instead use a pickaxe Breaking by hand is still viable

--- src/main/java/com/lance5057/extradelight/data/EDBlockTags.java
+++ src/main/java/com/lance5057/extradelight/data/EDBlockTags.java
@@ -44,6 +44,7 @@ public class EDBlockTags extends BlockTagsProvider {
 //		);
 
 		tag(BlockTags.MINEABLE_WITH_PICKAXE).add(
+				//Normal blocks
 				ExtraDelightBlocks.OVEN.get(),
 				ExtraDelightBlocks.MORTAR_STONE.get(),
 				ExtraDelightBlocks.MORTAR_ANDESITE.get(),
@@ -54,16 +55,74 @@ public class EDBlockTags extends BlockTagsProvider {
 				ExtraDelightBlocks.MORTAR_BASALT.get(),
 				ExtraDelightBlocks.MORTAR_ENDSTONE.get(),
 				ExtraDelightBlocks.MORTAR_AMETHYST.get(),
-				ExtraDelightBlocks.MORTAR_GILDED_BLACKSTONE.get()
+				ExtraDelightBlocks.MORTAR_GILDED_BLACKSTONE.get(),
+
+				//Feasts in pans
+				ExtraDelightBlocks.LASAGNA.get(),
+				ExtraDelightBlocks.HOTDISH.get(),
+
+				//Feasts in pots
+				ExtraDelightBlocks.MACARONI_CHEESE.get(),
+				ExtraDelightBlocks.CURRY.get(),
+				ExtraDelightBlocks.BEEF_STEW.get(),
+				ExtraDelightBlocks.PORK_STEW.get(),
+				ExtraDelightBlocks.LAMB_STEW.get(),
+				ExtraDelightBlocks.RABBIT_STEW.get(),
+				ExtraDelightBlocks.CHICKEN_STEW.get(),
+				ExtraDelightBlocks.FISH_STEW.get()
 		);
 
 //		tag(net.minecraft.tags.BlockTags.MINEABLE_WITH_SHOVEL).add(
 //
 //		);
 
-//		tag(ModTags.MINEABLE_WITH_KNIFE).add(
-//
-//		);
+		tag(ModTags.MINEABLE_WITH_KNIFE).add(
+				//Pies and plain cake
+				ExtraDelightBlocks.SWEET_BERRY_PIE.get(),
+				ExtraDelightBlocks.GLOW_BERRY_PIE.get(),
+				ExtraDelightBlocks.CHEESECAKE.get(),
+				ExtraDelightBlocks.HONEY_CHEESECAKE.get(),
+				ExtraDelightBlocks.CHOCOLATE_CHEESECAKE.get(),
+				ExtraDelightBlocks.PUMPKIN_CHEESECAKE.get(),
+				ExtraDelightBlocks.GLOW_BERRY_CHEESECAKE.get(),
+				ExtraDelightBlocks.PLAIN_CAKE.get(), //This may cause issues as it doesn't exist ingame
+				ExtraDelightBlocks.QUICHE.get(),
+				ExtraDelightBlocks.MEAT_PIE_BLOCK.get(),
+
+				//Feasts in bowls
+				ExtraDelightBlocks.SALISBURY_STEAK_FEAST.get(),
+				ExtraDelightBlocks.MASHED_POTATO_GRAVY.get(),
+				ExtraDelightBlocks.HASH_FEAST.get(),
+				ExtraDelightBlocks.PULLED_PORK_FEAST.get(),
+				ExtraDelightBlocks.STIRFRY.get(),
+				ExtraDelightBlocks.SALAD.get(),
+
+				//Feasts on plates
+				ExtraDelightBlocks.MEATLOAF_FEAST.get(),
+				ExtraDelightBlocks.POT_ROAST_FEAST.get(),
+				ExtraDelightBlocks.BBQ_RIBS_FEAST.get(),
+				ExtraDelightBlocks.RACK_LAMB.get(),
+				ExtraDelightBlocks.BEEF_WELLINGTON.get(),
+				ExtraDelightBlocks.HAGGIS.get(),
+
+				//Jellies (also plated)
+				ExtraDelightBlocks.JELLY_WHITE.get(),
+				ExtraDelightBlocks.JELLY_ORANGE.get(),
+				ExtraDelightBlocks.JELLY_MAGENTA.get(),
+				ExtraDelightBlocks.JELLY_LIGHT_BLUE.get(),
+				ExtraDelightBlocks.JELLY_YELLOW.get(),
+				ExtraDelightBlocks.JELLY_LIME.get(),
+				ExtraDelightBlocks.JELLY_PINK.get(),
+				ExtraDelightBlocks.JELLY_GREY.get(),
+				ExtraDelightBlocks.JELLY_LIGHT_GREY.get(),
+				ExtraDelightBlocks.JELLY_CYAN.get(),
+				ExtraDelightBlocks.JELLY_PURPLE.get(),
+				ExtraDelightBlocks.JELLY_BLUE.get(),
+				ExtraDelightBlocks.JELLY_BROWN.get(),
+				ExtraDelightBlocks.JELLY_GREEN.get(),
+				ExtraDelightBlocks.JELLY_RED.get(),
+				ExtraDelightBlocks.JELLY_BLACK.get()
+		);
 		AestheticBlocks.WALLPAPER_BLOCKS.forEach(this::makeMineableWithKnife);
 		AestheticBlocks.MOLDED_WALLPAPER_BLOCKS.forEach(this::makeMineableWithKnife);
 
