3a7c2987c695c23eb72fae02ec9b0cfa88d6e995
Lance5057 <Lance5057@gmail.com>
2023-09-26 22:42:34 -0500

    Maze Grottos

--- src/main/java/com/lance5057/extradelight/ExtraDelight.java
+++ src/main/java/com/lance5057/extradelight/ExtraDelight.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(ExtraDelight.MOD_ID)
 public class ExtraDelight {
 	public final static String MOD_ID = "extradelight";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3_test";
 
 	public static Logger logger = LogManager.getLogger();
 
--- src/main/java/com/lance5057/extradelight/worldgen/features/CornMazeFeature.java
+++ src/main/java/com/lance5057/extradelight/worldgen/features/CornMazeFeature.java
@@ -1,11 +1,16 @@
 package com.lance5057.extradelight.worldgen.features;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import com.lance5057.extradelight.blocks.crops.corn.CornBottom;
 import com.lance5057.extradelight.util.EllersGen;
 import com.mojang.serialization.Codec;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -16,6 +21,8 @@ public class CornMazeFeature extends Feature<SimpleBlockConfiguration> {
 	public CornMazeFeature(Codec<SimpleBlockConfiguration> p_66808_) {
 		super(p_66808_);
 	}
+
+	List<Block> expected = Arrays.asList(Blocks.STONE, Blocks.AIR);// ExtraDelightBlocks.CORN_BOTTOM.get();
 
 	/**
 	 * Places the given feature at the given location. During world generation,
@@ -47,26 +54,44 @@ public class CornMazeFeature extends Feature<SimpleBlockConfiguration> {
 		char[][] m = maze.getMaze();
 
 		// scan area
-		for (int x = 0; x < w; x++) {
-			for (int z = 0; z < h; z++) {
+		for (int x = 0; x < w - 2; x++) {
+			for (int z = 0; z < h - 2; z++) {
 				BlockPos p = new BlockPos(blockpos.getX() + x, blockpos.getY(), blockpos.getZ() + z);
-				if (!worldgenlevel.isEmptyBlock(p)) {
-					return false;
+				// if (worldgenlevel.getBlockState(p).is(ExtraDelightBlocks.CORN_BOTTOM.get()))
+				// {
+
+//				if (worldgenlevel.getBlockState(p).is(expected)) {
+//					return false;
+//				}
+			}
+		}
+
+		for (int x = 1; x < w - 1; x++) {
+			for (int z = 1; z < h - 1; z++) {
+				if (blockstate.canSurvive(worldgenlevel, blockpos)) {
+					if (blockstate.getBlock() instanceof CornBottom) {
+						BlockPos p = new BlockPos(blockpos.getX() + x - 1, blockpos.getY(), blockpos.getZ() + z - 1);
+
+						if (m[x][z] == '#' && worldgenlevel.canSeeSky(p))
+							if (checkAround(worldgenlevel, p, 3))
+								worldgenlevel.setBlock(p, Blocks.STONE.defaultBlockState(), 0);
+						// CornBottom.placeAt(worldgenlevel, blockstate, p, 0);
+					}
 				}
 			}
 		}
 
-		for (int x = 1; x < w-1 ; x++) {
-			for (int z = 1; z < h-1 ; z++) {
-				if (blockstate.canSurvive(worldgenlevel, blockpos)) {
-					if (blockstate.getBlock() instanceof CornBottom) {
-						BlockPos p = new BlockPos(blockpos.getX() + x-1, blockpos.getY(), blockpos.getZ() + z-1);
+		return true;
+	}
 
-						if (m[x][z] == '#')
-							worldgenlevel.setBlock(p, Blocks.STONE.defaultBlockState(), 0);
-						// CornBottom.placeAt(worldgenlevel, blockstate, p, 0);
-					}
-				}
+	boolean checkAround(WorldGenLevel level, BlockPos pos, int size) {
+		for (int x = -size; x < size; x++) {
+			for (int z = -size; z < size; z++) {
+				BlockPos p = new BlockPos(pos.getX() + x, pos.getY(), pos.getZ() + z);
+
+				if (!expected.contains(level.getBlockState(p).getBlock()))
+					return false;
+
 			}
 		}
 
