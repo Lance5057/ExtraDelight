972578855abacfcc443081cd64f2e634c1fe5c8e
Lance5057 <Lance5057@gmail.com>
2024-01-21 17:04:41 -0600

    Glowberry Juice Patchouli

--- src/main/java/com/lance5057/extradelight/data/PatchouliGen.java
+++ src/main/java/com/lance5057/extradelight/data/PatchouliGen.java
@@ -3,8 +3,10 @@ package com.lance5057.extradelight.data;
 import java.util.function.Consumer;
 
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
 import xyz.brassgoggledcoders.patchouliprovider.BookBuilder;
 import xyz.brassgoggledcoders.patchouliprovider.CategoryBuilder;
+import xyz.brassgoggledcoders.patchouliprovider.EntryBuilder;
 import xyz.brassgoggledcoders.patchouliprovider.PatchouliBookProvider;
 
 public class PatchouliGen extends PatchouliBookProvider {
@@ -21,20 +23,104 @@ public class PatchouliGen extends PatchouliBookProvider {
 						"This is a WIP, many pages will contain duplicates!")
 				.setBookTexture("patchouli:textures/gui/book_red.png").setModel("extradelight:cookbook");
 
-		CategoryBuilder beverages = book.addCategory("beverages", "Beverages", "Drinks!", "extradelight:sweet_berry_juice").setSortnum(6);
-		CategoryBuilder casseroles = book.addCategory("casseroles", "Casseroles / One Pot", "Casseroles!", "extradelight:hotdish");
-		CategoryBuilder condiments = book.addCategory("condiments", "Condiments", "Saucy!", "extradelight:bbq_jar_item").setSortnum(7);
-		CategoryBuilder deserts = book.addCategory("deserts", "Deserts", "Sweet Treats!", "extradelight:chocolate_custard").setSortnum(5);
+		CategoryBuilder beverages = book
+				.addCategory("beverages", "Beverages", "Drinks!", "extradelight:sweet_berry_juice").setSortnum(6);
+		CategoryBuilder casseroles = book.addCategory("casseroles", "Casseroles / One Pot", "Casseroles!",
+				"extradelight:hotdish");
+		CategoryBuilder condiments = book.addCategory("condiments", "Condiments", "Saucy!", "extradelight:bbq_jar_item")
+				.setSortnum(7);
+		CategoryBuilder deserts = book
+				.addCategory("deserts", "Deserts", "Sweet Treats!", "extradelight:chocolate_custard").setSortnum(5);
 		CategoryBuilder eggs = book.addCategory("eggs", "Egg Dishes", "Breakfast!", "extradelight:quiche");
 		CategoryBuilder fish = book.addCategory("fish", "Fish and Seafood", "Fish!", "extradelight:fried_fish");
-		CategoryBuilder meats = book.addCategory("meats", "Meats and Cheeses", "Tasty Animals!", "extradelight:potroast");
+		CategoryBuilder meats = book.addCategory("meats", "Meats and Cheeses", "Tasty Animals!",
+				"extradelight:potroast");
 		CategoryBuilder pasta = book.addCategory("pasta", "Rice and Pasta", "Carbs!", "extradelight:cooked_pasta");
 		CategoryBuilder salads = book.addCategory("salads", "Salads", "Leafy!", "extradelight:salad");
-		CategoryBuilder sandwiches = book.addCategory("sandwiches", "Sandwiches", "Meals between bread!", "extradelight:fish_salad_sandwich");
+		CategoryBuilder sandwiches = book.addCategory("sandwiches", "Sandwiches", "Meals between bread!",
+				"extradelight:fish_salad_sandwich");
 		CategoryBuilder soups = book.addCategory("soups", "Soups", "Wet Food!", "extradelight:tomato_soup");
-		CategoryBuilder vegetables = book.addCategory("vegetables", "Fruits and Vegetables", "Tasty Plants!", "extradelight:glazed_carrot");
+		CategoryBuilder vegetables = book.addCategory("vegetables", "Fruits and Vegetables", "Tasty Plants!",
+				"extradelight:glazed_carrot");
+
+		doBeverages(beverages);
+		doCasseroles(casseroles);
+		doCondiments(condiments);
+		doDeserts(deserts);
+		doEggs(eggs);
+		doFish(fish);
+		doMeats(meats);
+		doPasta(pasta);
+		doSalads(salads);
+		doSandwiches(sandwiches);
+		doSoups(soups);
+		doVegetables(vegetables);
 
 		book.build(consumer);
+	}
+
+	private void doVegetables(CategoryBuilder vegetables) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doSoups(CategoryBuilder soups) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doSandwiches(CategoryBuilder sandwiches) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doSalads(CategoryBuilder salads) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doPasta(CategoryBuilder pasta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doMeats(CategoryBuilder meats) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doFish(CategoryBuilder fish) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doEggs(CategoryBuilder eggs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doDeserts(CategoryBuilder deserts) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doCondiments(CategoryBuilder condiments) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doCasseroles(CategoryBuilder casseroles) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void doBeverages(CategoryBuilder beverages) {
+		EntryBuilder glowberry = beverages.addEntry("glow_berry_juice", "Glow Berry Juice",
+				"extradelight:glow_berry_juice");
+		glowberry.addTextPage(
+				"A tart juice made from glowberries that maintains its glow inducing quality. It's anti-inflammatory and anti-oxidant effects will make you feel a little healthier almost instantly!");
+		glowberry.addCraftingPage(new ResourceLocation("extradelight", "glow_berry_juice"));
+		glowberry.build();
 	}
 
 }
