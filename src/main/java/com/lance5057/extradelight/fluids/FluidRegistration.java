3122bfe785f2e546cc6aa50e12e0310ad9c284d5
Lance5057 <Lance5057@gmail.com>
2025-03-01 02:16:58 -0600

    create

--- src/main/java/com/lance5057/extradelight/fluids/FluidRegistration.java
+++ src/main/java/com/lance5057/extradelight/fluids/FluidRegistration.java
@@ -37,9 +37,16 @@ public class FluidRegistration {
 			Supplier<? extends Item> bucket, DeferredRegister<FluidType> FLUID_TYPES, DeferredRegister<Fluid> FLUIDS, int decrease) {
 		TYPE = FLUID_TYPES.register(name + "_fluid", fluid);
 
-//		BaseFlowingFluid.Properties p = properties(block, bucket);
-
 		FLUID = FLUIDS.register(name + "_fluid", () -> new BaseFlowingFluid.Source(properties(block, bucket)));
 		FLUID_FLOWING = FLUIDS.register(name + "_fluid_flowing", () -> new BaseFlowingFluid.Flowing(properties(block, bucket).levelDecreasePerBlock(decrease)));
+	}
+	
+	public FluidRegistration(String name, Supplier<? extends FluidType> fluid, Supplier<? extends LiquidBlock> block,
+			Supplier<? extends Item> bucket, DeferredRegister<FluidType> FLUID_TYPES, DeferredRegister<Fluid> FLUIDS, int decrease, int findSlope) {
+		TYPE = FLUID_TYPES.register(name + "_fluid", fluid);
+
+		FLUID = FLUIDS.register(name + "_fluid", () -> new BaseFlowingFluid.Source(properties(block, bucket)));
+		FLUID_FLOWING = FLUIDS.register(name + "_fluid_flowing", () -> new BaseFlowingFluid.Flowing(
+				properties(block, bucket).levelDecreasePerBlock(decrease).slopeFindDistance(findSlope)));
 	}
 }
