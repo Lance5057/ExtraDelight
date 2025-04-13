22ea52790a5f1eb27763e3e08c0879abf4649ead
Lance5057 <Lance5057@gmail.com>
2025-04-13 14:23:50 -0500

    BottleFluidRegistry

---
+++ src/main/java/com/lance5057/extradelight/util/BottleFluidRegistry.java
@@ -0,0 +1,65 @@
+package com.lance5057.extradelight.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.lance5057.extradelight.ExtraDelightFluids;
+import com.lance5057.extradelight.ExtraDelightItems;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.material.Fluids;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.crafting.SizedFluidIngredient;
+
+public class BottleFluidRegistry {
+	private static int bottleMB = 250;
+
+	public static List<BottleFluid> registry = new ArrayList<BottleFluid>();
+
+	public static void register(Ingredient bottle, SizedFluidIngredient fluid) {
+		registry.add(new BottleFluid(bottle, fluid));
+	}
+
+	public static ItemStack getBottleFromFluid(FluidStack f) {
+		if (f.getAmount() >= bottleMB) {
+			Optional<BottleFluid> b = registry.stream().filter(bf -> bf.fluid.test(f)).findFirst();
+			if (b.isPresent()) {
+				return b.get().bottle.getItems()[0];
+			}
+		}
+		return ItemStack.EMPTY;
+	}
+
+	public static FluidStack getFluidFromBottle(ItemStack i) {
+		Optional<BottleFluid> b = registry.stream().filter(bf -> bf.bottle.test(i)).findFirst();
+		if (b.isPresent()) {
+			return b.get().fluid.getFluids()[0];
+		}
+		return FluidStack.EMPTY;
+	}
+
+	public static class BottleFluid {
+		Ingredient bottle;
+		SizedFluidIngredient fluid;
+
+		public BottleFluid(Ingredient b, SizedFluidIngredient f) {
+			this.bottle = b;
+			this.fluid = f;
+		}
+	}
+
+	static {
+		register(Ingredient.of(ExtraDelightItems.BLOOD_CHOCOLATE_SYRUP_BOTTLE.get()),
+				SizedFluidIngredient.of(ExtraDelightFluids.BLOOD_CHOCOLATE_SYRUP.FLUID.get(), bottleMB));
+		register(Ingredient.of(ExtraDelightItems.COCOA_BUTTER_BOTTLE.get()),
+				SizedFluidIngredient.of(ExtraDelightFluids.COCOA_BUTTER.FLUID.get(), bottleMB));
+
+		register(Ingredient.of(PotionContents.createItemStack(Items.POTION, Potions.WATER)),
+				SizedFluidIngredient.of(Fluids.WATER, bottleMB));
+	}
+}
--- src/main/java/com/lance5057/extradelight/workstations/vat/VatBlockEntity.java
+++ src/main/java/com/lance5057/extradelight/workstations/vat/VatBlockEntity.java
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import com.lance5057.extradelight.ExtraDelightBlockEntities;
 import com.lance5057.extradelight.ExtraDelightRecipes;
 import com.lance5057.extradelight.util.BlockEntityUtils;
+import com.lance5057.extradelight.util.BottleFluidRegistry;
 import com.lance5057.extradelight.workstations.vat.recipes.VatRecipe;
 import com.lance5057.extradelight.workstations.vat.recipes.VatRecipeWrapper;
 
@@ -125,13 +126,21 @@ public class VatBlockEntity extends BlockEntity {
 					bowl.items.setStackInSlot(LIQUID_IN_SLOT, Items.BUCKET.getDefaultInstance());
 
 				}
-			} else {
+			} else if (inputItem.getCapability(Capabilities.FluidHandler.ITEM) != null) {
 				IFluidHandlerItem fluidHandlerItem = inputItem.getCapability(Capabilities.FluidHandler.ITEM);
 				int filled = FluidUtil.tryFluidTransfer(bowl.getFluidTank(), fluidHandlerItem,
 						bowl.getFluidTank().getFluidAmount(), true).getAmount();
 				if (filled > 0) {
 					bowl.items.setStackInSlot(LIQUID_IN_SLOT, fluidHandlerItem.getContainer());
 
+				}
+			} else {
+				FluidStack f = BottleFluidRegistry.getFluidFromBottle(inputItem);
+				if (!f.isEmpty()) {
+					if (bowl.getFluidTank().fill(f, FluidAction.SIMULATE) == 250) {
+						bowl.getFluidTank().fill(f, FluidAction.EXECUTE);
+						bowl.items.setStackInSlot(LIQUID_IN_SLOT, new ItemStack(Items.GLASS_BOTTLE, 1));
+					}
 				}
 			}
 		}
@@ -148,12 +157,20 @@ public class VatBlockEntity extends BlockEntity {
 					inputItem.shrink(1);
 					bowl.items.setStackInSlot(LIQUID_OUT_SLOT, stack.getFluid().getBucket().getDefaultInstance());
 				}
-			} else {
+			} else if (inputItem.getCapability(Capabilities.FluidHandler.ITEM) != null) {
 				IFluidHandlerItem fluidHandlerItem = inputItem.getCapability(Capabilities.FluidHandler.ITEM);
 				int filled = FluidUtil.tryFluidTransfer(fluidHandlerItem, bowl.getFluidTank(),
 						bowl.getFluidTank().getFluidAmount(), true).getAmount();
 				if (filled > 0) {
 					bowl.items.setStackInSlot(LIQUID_OUT_SLOT, fluidHandlerItem.getContainer());
+				}
+			} else {
+				ItemStack i = BottleFluidRegistry.getBottleFromFluid(bowl.getFluidTank().getFluid());
+				if (!i.isEmpty()) {
+					if (bowl.getFluidTank().drain(250, FluidAction.SIMULATE).getAmount() == 250) {
+						bowl.getFluidTank().drain(250, FluidAction.EXECUTE);
+						bowl.items.setStackInSlot(LIQUID_OUT_SLOT, i);
+					}
 				}
 			}
 		}
@@ -175,16 +192,16 @@ public class VatBlockEntity extends BlockEntity {
 
 			@Override
 			public boolean isItemValid(int slot, ItemStack stack) {
-				if (slot == LIQUID_IN_SLOT)
-					if (stack.getCapability(Capabilities.FluidHandler.ITEM) != null)
-						return true;
-					else
-						return false;
-				if (slot == LIQUID_OUT_SLOT)
-					if (stack.getCapability(Capabilities.FluidHandler.ITEM) != null)
-						return true;
-					else
-						return false;
+//				if (slot == LIQUID_IN_SLOT)
+//					if (stack.getCapability(Capabilities.FluidHandler.ITEM) != null)
+//						return true;
+//					else
+//						return false;
+//				if (slot == LIQUID_OUT_SLOT)
+//					if (stack.getCapability(Capabilities.FluidHandler.ITEM) != null)
+//						return true;
+//					else
+//						return false;
 //				if (slot == OUTPUT_SLOT)
 //					return false;
 				return true;
