440854cc815bd7f110a55db5083fe76499432d77
uuiip7878 <55552793+uuiip7878@users.noreply.github.com>
2025-06-05 22:15:30 +0900

    fix mixing bowl, add yeast info for jei
    now it shouldn't get messed up with certain fluid io requests

--- src/main/java/com/lance5057/extradelight/workstations/mixingbowl/MixingBowlBlockEntity.java
+++ src/main/java/com/lance5057/extradelight/workstations/mixingbowl/MixingBowlBlockEntity.java
@@ -99,7 +99,7 @@ public class MixingBowlBlockEntity extends BlockEntity {
 				if (filled == FluidType.BUCKET_VOLUME) {
 					bowl.getFluidTank().fill(new FluidStack(filledBucket.content, FluidType.BUCKET_VOLUME),
 							IFluidHandler.FluidAction.EXECUTE);
-					BlockEntityUtils.Inventory.dropItemInWorld(Items.BUCKET.getDefaultInstance(), bowl.level,
+					BlockEntityUtils.Inventory.dropItemInWorld(Items.BUCKET.getDefaultInstance().copyWithCount(1), bowl.level,
 							bowl.worldPosition);
 
 					bowl.items.getStackInSlot(LIQUID_IN_SLOT).shrink(1);
@@ -126,7 +126,7 @@ public class MixingBowlBlockEntity extends BlockEntity {
 									bowl.level, bowl.worldPosition);
 
 						} else {
-							BlockEntityUtils.Inventory.dropItemInWorld(inputItem.getCraftingRemainingItem().copy(),
+							BlockEntityUtils.Inventory.dropItemInWorld(inputItem.getCraftingRemainingItem().copyWithCount(sz),
 									bowl.level, bowl.worldPosition);
 						}
 
@@ -163,20 +163,22 @@ public class MixingBowlBlockEntity extends BlockEntity {
 				if (bowl.getFluidTank().getFluid() != null) {
 					FluidStack stack = bowl.getFluidTank().drain(250, IFluidHandler.FluidAction.SIMULATE);
 					ItemStack i = BottleFluidRegistry.getBottleFromFluid(stack);
+					int sz = inputItem.getCount();
 					if (!i.isEmpty() && i.getItem().getCraftingRemainingItem() == inputItem.getItem()) {
-						int sz = inputItem.getCount();
 						for(int j=0;j<sz;j++)
 							bowl.getFluidTank().drain(stack, FluidAction.EXECUTE);
 						BlockEntityUtils.Inventory.dropItemInWorld(i.copyWithCount(sz), bowl.level, bowl.worldPosition);
-						inputItem.shrink(sz);
 //						bowl.items.setStackInSlot(LIQUID_OUT_SLOT, i);
 					}
 					// Because the blasted water bottle has no craftRemainder
 					if (i.getItem() == Items.POTION && inputItem.getItem() == Items.GLASS_BOTTLE) {
 						FluidStack stack1 = bowl.getFluidTank().drain(250, IFluidHandler.FluidAction.SIMULATE);
-						bowl.getFluidTank().drain(stack1, FluidAction.EXECUTE);
-						BlockEntityUtils.Inventory.dropItemInWorld(i, bowl.level, bowl.worldPosition);
+						for(int j=0;j<sz;j++) {
+							bowl.getFluidTank().drain(stack1, FluidAction.EXECUTE);
+							BlockEntityUtils.Inventory.dropItemInWorld(i, bowl.level, bowl.worldPosition);
+						}
 					}
+					inputItem.shrink(sz);
 					bowl.updateInventory();
 				}
 			}
@@ -212,13 +214,13 @@ public class MixingBowlBlockEntity extends BlockEntity {
 				}
 			}
 
-			@Override
-			protected int getStackLimit(int slot, @Nonnull ItemStack stack) {
-				if (slot == LIQUID_IN_SLOT || slot == LIQUID_OUT_SLOT)
-					return 1;
-				else
-					return 64;
-			}
+//			@Override
+//			protected int getStackLimit(int slot, @Nonnull ItemStack stack) {
+//				if (slot == LIQUID_IN_SLOT || slot == LIQUID_OUT_SLOT)
+//					return 1;
+//				else
+//					return 64;
+//			}
 
 			@Override
 			public boolean isItemValid(int slot, ItemStack stack) {
@@ -227,11 +229,18 @@ public class MixingBowlBlockEntity extends BlockEntity {
 						FluidStack ftmp = BottleFluidRegistry.getFluidFromBottle(stack.copyWithCount(1));
 						return (stack.getCount() + items.getStackInSlot(slot).getCount() < 2) || (fluids.fill(ftmp, FluidAction.SIMULATE) == 250 && stack.getItem() != Items.POTION);
 					case LIQUID_OUT_SLOT:
+						// extra notable things to care for LIQUID_OUT_SLOT
+						// - water bottles dont stack
+						// - antecedent is currently hardcoded; it wont be flexible against
+						//   any fluid container items added by other mods
+						if (fluids.getTanks() == 0)return false;
 						ItemStack itmp = BottleFluidRegistry.getBottleFromFluid(fluids.getFluid(0).copyWithAmount(250));
 						boolean antecedent = (!itmp.isEmpty() && itmp.getItem().getCraftingRemainingItem() == stack.getItem())
-											|| (itmp.getItem() == Items.POTION && stack.getItem() == Items.GLASS_BOTTLE);
+											|| (itmp.getItem() == Items.POTION && stack.getItem() == Items.GLASS_BOTTLE)
+											|| (stack.getItem() == Items.BUCKET && fluids.getFluidAmount(0) >= 1000);
 						return (antecedent) &&
-								(!itmp.isEmpty() || stack.getCount() + items.getStackInSlot(slot).getCount() < 2);
+								(!itmp.isEmpty() 
+								 || stack.getCount() + items.getStackInSlot(slot).getCount() < 2);
 					case GHOST_SLOT:
 						return false;
 					default:
