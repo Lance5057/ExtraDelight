8bbd7fdf97cdf0e408f7ffbfc109712c9f06a258
Robynstar <robynstar_petirrojo@hotmail.com>
2025-06-24 22:00:00 +1200

    Fix Bar Mould item model and vat gui container icons.

--- src/main/java/com/lance5057/extradelight/data/ItemModels.java
+++ src/main/java/com/lance5057/extradelight/data/ItemModels.java
@@ -1168,7 +1168,9 @@ public class ItemModels extends ItemModelProvider {
 		forBlockItem(ExtraDelightItems.HAZELNUT_LEAVES, modLoc("block/crops/fruit/hazelnut/hazelnut_leaves_stage0"));
 
 		forBlockItem(this, ExtraDelightItems.CHILLER, "chiller");
-		forBlockItem(this, ExtraDelightItems.BAR_MOLD, "bar_mold");
+		this.getBuilder(ExtraDelightItems.BAR_MOLD.getId().getPath())
+				.parent(new ModelFile.UncheckedModelFile("block/block"))
+				.customLoader(BlockStateItemGeometryLoader::builder);
 		forBlockItemFlat(this, ExtraDelightItems.HAZELNUT_SAPLING, "crops/fruit/hazelnut/hazelnut_sapling");
 
 		forItem(this, ExtraDelightItems.BLOOD_CHOCOLATE_SYRUP_FLUID_BUCKET, "blood_chocolate_syrup_bucket");
