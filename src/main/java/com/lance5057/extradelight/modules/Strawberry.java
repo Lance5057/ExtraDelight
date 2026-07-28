eda258cb4d99edab4cffe0ef1369a999b2929a0b
Lance5057 <Lance5057@gmail.com>
2026-07-28 15:41:12 -0500

    3d bottles

---
+++ src/main/java/com/lance5057/extradelight/modules/Strawberry.java
@@ -0,0 +1,16 @@
+package com.lance5057.extradelight.modules;
+
+import static vectorwing.farmersdelight.common.registry.ModItems.foodItem;
+
+import com.lance5057.extradelight.food.EDFoods;
+import com.lance5057.extradelight.items.ToolTipConsumableItem;
+import com.lance5057.extradelight.util.EDItemGenerator;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+public class Strawberry {
+//	public static final DeferredItem<Item> STRAWBERRY = EDItemGenerator
+//			.register("strawberry", () -> new ToolTipConsumableItem(foodItem(EDFoods.STRAWBERRY), true)).advancementIngredients()
+//			.finish();
+}
